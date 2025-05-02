3d93b6ae238544612b2fa0e13d92aa46ff519548
arash <arash.golrokhian@hotmail.com>
2025-05-03 02:01:14 +0330

    added media

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
 
   // Enable validation
   app.useGlobalPipes(
